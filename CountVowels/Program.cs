1a6aa5c7a5ef331de367d17512f2444423e56603
Tauriqh <37148245+Tauriqh@users.noreply.github.com>
2019-02-21 14:27:04 +0200

    Add files via upload

---
+++ CountVowels/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int count = 0;
+
+            Console.Write("Please enter the word!");
+            word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                switch (word[i])
+                {
+                    case 'a': count++; break;
+                    case 'e': count++; break;
+                    case 'i': count++; break;
+                    case 'o': count++; break;
+                    case 'u': count++; break;
+                    case 'A': count++; break;
+                    case 'E': count++; break;
+                    case 'I': count++; break;
+                    case 'O': count++; break;
+                    case 'U': count++; break;
+                }
+            }
+
+            Console.WriteLine("The number of vowels is {0}",count);
+        }
+    }
+}
